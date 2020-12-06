133d6fcfe722cceeaca4e30e9ee29dd8e6e21ca8
Mathieu Ledru <matyo91@gmail.com>
2020-12-07 00:07:48 +0100

    :heavy_plus_sign: setup cargo-aoc project

---
+++ README.md
@@ -0,0 +1,6 @@
+AAdvent of Code
+--------------
+
+More informations at https://adventofcode.com/
+
+This project is based on template from [cargo-aoc](https://github.com/gobanos/cargo-aoc)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate adventofcode;
+extern crate aoc_runner_derive;
+extern crate aoc_runner;
+
+use aoc_runner_derive::aoc_main;
+
+aoc_main! { lib = adventofcode }
